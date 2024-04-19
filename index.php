8e7fc212b744e461fd899c5ffb86cedc2a5ed62b
Lại Tiến Đức <94907773+Tienduc37y@users.noreply.github.com>
2024-04-19 08:43:56 +0000

    bt for bt truyền data và readme phân biệt dom

--- index.php
+++ index.php
@@ -1,23 +1,31 @@
-<?php
 
-function sayHello($name) {
-	echo "Hello $name!";
-}
-
-?>
 
 <html>
 	<head>
-		<title>Advanced Web Development K15-N03</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>Visual Studio Code Remote :: PHP</title>
 	</head>
 	<body>
-		<?php 
-		sayHello('K15-N03 2024');		
+		<h2>Nhóm 17</h2>
+		<?php
+		for ($i = 1; $i <= 200; $i++) {
+			if ($i % 2 == 0) {
+				echo '<span style="color: red; font-weight: bold;">' . $i . '</span> ';
+			} else {
+				echo '<span style="color: blue; font-style: italic;">' . $i . '</span> ';
+			}
+		}
 		?>
-		<form action="checkLogin.php" method="POST">
-		<a>Username:</a><input type=text name="userName" size =16>
-		<a>Password:</a><input type=password name="passWord" size =16>
-		<input type=submit name=submit value="Login">
-		</form>
+		<div>
+			<h1>Đăng nhập hệ thống</h1>
+			<form action="info.php" method="get">
+				<label for="username">Tên đăng nhập : </label>
+				<input type="text" name="username"> <br><br>
+				<label for="password">Mật khẩu:</label>
+				<input type="password" name="password"><br><br>
+				<input type="submit">
+			</form>
+		</div>
+		
 	</body>
 </html>
