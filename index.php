16290b5c49277067163ae99a47a6759a65806e91
Lại Tiến Đức <94907773+Tienduc37y@users.noreply.github.com>
2024-04-22 01:25:13 +0000

    vd array

--- index.php
+++ index.php
@@ -1,32 +1,27 @@
-<?php
 
-///*----------------------------------------------------------------------------------------
-// * Copyright (c) Microsoft Corporation. All rights reserved.
-// * Licensed under the MIT License. See LICENSE in the project root for license information.
-// *---------------------------------------------------------------------------------------*/
-
-function sayHello($name) {
-	echo "Hello $name!";
-}
-
-?>
 
 <html>
 	<head>
-		<title>Advanced Web Development K15-N03</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>Visual Studio Code Remote :: PHP</title>
 	</head>
 	<body>
-		<?php 
-		
-		sayHello('K15-N03 2024');
-			
-	
-			
+		<h2>Nhóm 17</h2>
+		<?php
+			$n = 100;
+			$arr1 = array($n);
+			$arr2 = array(1,2,3,4);
+			$arr3 = array();
+			$arr4 = array("hoten" => "Nam Khanhs",
+						"quequan" => "Ha Nội",
+						"tuoi" => 22			
+			);
+			print_r($n);
+			print_r($arr1);
+			print_r($arr2);
+			print_r($arr3);
+			print_r($arr4);
 		?>
-		<form action="checkLogin.phd" method="POST">
-		<a>Username:</a><input type=text name="userName" size =16>
-		<a>Password:</a><input type=password name="passWord" size =16>
-		<input type=submit name=submit value="Login">
-		</form>
+		
 	</body>
 </html>
