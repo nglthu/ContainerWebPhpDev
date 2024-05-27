0ff642278c58fdc872efeb51593d8f4ca6252b41
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2024-05-27 08:39:18 +0000

    test

--- Controller/getData.php
+++ Controller/getData.php
@@ -1,8 +1,10 @@
 <?php
 //require_once './workspaces/ContainerWebPhpDev/Model/Database.php';
+$test = require './/Model/Database.php';
 
-$test =getcwd();
+//$test =getcwd();
 /*
+/workspaces/ContainerWebPhpDev/Controller
 $conn = new Database();
 $conn->dBConnection();*/
 //get api
