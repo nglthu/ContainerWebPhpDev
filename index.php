0efbea8ad171bcfde36f0eeb327c8526fff39169
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2024-06-17 10:17:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,10 +14,6 @@ function sayHello($name) {
 		<?php 
 		sayHello('everyone');		
 		?>
-		<form action="checkLogin.php" method="POST">
-		<a>Username:</a><input type=text name="userName" size =16>
-		<a>Password:</a><input type=password name="passWord" size =16>
-		<input type=submit name=submit value="Login">
-		</form>
+		
 	</body>
 </html>
