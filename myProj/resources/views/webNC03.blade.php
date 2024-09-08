7be7107d7555a4becdb07797b88eea6ee12ab5f0
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2024-09-08 11:48:27 +0000

    test webNC03 route

---
+++ myProj/resources/views/webNC03.blade.php
@@ -0,0 +1,6 @@
+<html>
+
+<body>
+    <h1>Hello web nang cao N03 </h1>
+</body>
+</html>
--- myProj/routes/web.php
+++ myProj/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/webNC03', function () {
+    return view('webNC03');
+});
