b25e023246df9090b6c34ba4b4cb692d2751e582
lethunguyen <165644902+lethunguyen@users.noreply.github.com>
2024-10-01 07:59:41 +0000

    create_books_table

--- webProject/database/migrations/2024_10_01_065431_create_books_table.php
+++ webProject/database/migrations/2024_10_01_065431_create_books_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('BookID');
             $table->string('Title');
-           
             $table->timestamps();
         });
     }
