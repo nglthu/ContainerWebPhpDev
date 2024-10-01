b183d7b561c5ea7b9892d30fdb82231ee8e386a1
lethunguyen <165644902+lethunguyen@users.noreply.github.com>
2024-10-01 06:58:52 +0000

    Model-Seed-Table/Migrate

--- webProject/app/Models/Book.php
+++ webProject/app/Models/Book.php
@@ -7,5 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
+
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'BookID',
+        'Title',
+        'Created_at',
+    ];
+
 }
--- webProject/database/seeders/DatabaseSeeder.php
+++ webProject/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Book;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -18,6 +19,12 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        Book::factory()->create([
+            'BookID' => 'b1',
+            'Title' => 'test',
+            'Created_at'=>'',
         ]);
     }
 }
