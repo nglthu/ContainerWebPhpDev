71987c38239b09e4226a712cf273cb7224049042
lethunguyen <165644902+lethunguyen@users.noreply.github.com>
2024-10-06 14:50:43 +0000

    bootstrap

--- webProject/database/seeders/DatabaseSeeder.php
+++ webProject/database/seeders/DatabaseSeeder.php
@@ -34,11 +34,20 @@ class DatabaseSeeder extends Seeder
             'dateOfBirth' => '7/7/2006',
             'mark1' => 9,
             'mark2' => 3,
-            'gpa'=> 0,],
-            
-            ['firstname'=>'Nam','lastname'=>'Tran','dateOfBirth'=>'3/4/2006','mark1'=>6,'mark2'=>1,'gpa'=>0,],
-            ['firstname'=>'Ha','lastname'=>'Le','dateOfBirth'=>'1/2/2006','mark1'=>5,'mark2'=>3,'gpa'=>0,],
-            ['firstname'=>'Luu','lastname'=>'Hoang','dateOfBirth'=>'2/2/2007','mark1'=>3,'mark2'=>2,'gpa'=>0,],
-            ['firstname'=>'Hoang','lastname'=>'Nguyen','dateOfBirth'=>'1/1/2005','mark1'=>10,'mark2'=>7,'gpa'=>0,]);
+            'gpa'=> 0,]);
+            Student::create([    
+            'firstname' => 'Nam','lastname' => 'Tran',
+'dateOfBirth' => '3/4/2006',
+'mark1' => 6,
+'mark2' => 1,
+'gpa' => 0,]);
+Student::create([
+    'firstname' => 'Ha',
+    'lastname' => 'Le',
+    'dateOfBirth' => '1/2/2006',
+    'mark1' => 5,
+    'mark2'=> 3,
+    'gpa' => 0,
+]);
     }
 }
--- webProject/resources/views/Student.blade.php
+++ webProject/resources/views/Student.blade.php
@@ -5,65 +5,113 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Web with Laravel </title>
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous">
+    </script>
+
+
 </head>
 
 <body>
 
-    <body>
-
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-            crossorigin="anonymous"></script>
-
-        <h1 class="text-center">Web-Laravel framework</h1>
-        <div>
-
-
-
-            <div>
-                @yield('content');
+    <div class="pos-f-t">
+        <div class="collapse" id="navbarToggleExternalContent">
+            <div class="bg-dark p-4">
+                <h4 class="text-white">Collapsed content</h4>
+                <span class="text-muted">Toggleable via the navbar brand.</span>
             </div>
+        </div>
+        <nav class="navbar navbar-dark bg-dark">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </nav>
+    </div>
 
-            <div class="container">
-                <div class="row align-items-start">
-                    <div class="col">
-                        Firstname
-                    </div>
-                    <div class="col">
-                        Lastname
-                    </div>
-                    <div class="col">
-                        Date Of Birth
-                    </div>
+
+
+
+
+    <div class="container">
+
+
+        <form>
+            <div class="row align-items-start">
+                <div class=".col-md-6">
+                    <input class="form-control mr-sm-1" type="search" placeholder="Search" aria-label="Search">
                 </div>
-                @foreach($students as $st)
-                <div class="row align-items-end">
-                    <div class="col">
-
-                        {{ $st->firstname}}
-
-                    </div>
-                    <div class="col">
-
-                        {{ $st->lastname}}
-
-                    </div>
-                    <div class="col">
-
-                        {{ $st->dateOfBirth}}
-
-                    </div>
+                <div class=".col-md-6">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                 </div>
-                @endforeach
             </div>
+        </form>
+    </div>
+    <div class="row align-items-start" style="background-color: green; color :white">
+    <div class="col" >
+           
+        </div>
+        <div class="col" >
+            Firstname
+        </div>
+        <div class="col">
+            Lastname
+        </div>
+        <div class="col" >
+            Date Of Birth
+        </div>
+        <div class="col" >
+           1st Mark 
+        </div>
+        <div class="col" >
+          2nd Mark
+        </div>
+        <div class="col" >
+          
+        </div>
+    </div>
+    @foreach($students as $st)
 
+
+    <div class="row align-items-end" style="background-color: #e3f2fd; color:green">
+    <div class="col" >
+          
+        </div>    
+    <div class="col">
+
+            {{ $st->firstname}}
 
         </div>
-    </body>
+        <div class="col">
+
+            {{ $st->lastname}}
+
+        </div>
+        <div class="col">
+
+            {{ $st->dateOfBirth}}
+
+        </div>
+        <div class="col">
+
+            {{ $st->mark1}}
+
+        </div>
+        <div class="col">
+
+            {{ $st->mark2}}
+
+        </div>
+        <div class="col" >
+          
+        </div>
+    </div>
+    @endforeach
+    </div>
+
+</body>
 
 </html>
