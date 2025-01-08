872fb8097114a024a1bb5f6510323b4081b314a2
lethunguyen <165644902+lethunguyen@users.noreply.github.com>
2025-01-08 10:48:46 +0000

    crsf

--- myWebApp/bootstrap/app.php
+++ myWebApp/bootstrap/app.php
@@ -10,26 +10,17 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
+    
 
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
-            'https://special-funicular-pj7qr4qv9r9x3vpq-8001.app.github.dev/*',
-            'https://special-funicular-pj7qr4qv9r9x3vpq-8001.app.github.dev',
+            'https://special-funicular-pj7qr4qv9r9x3vpq-8001.app.github.dev/postData',
+            'http://127.0.0.1/postData',
         ]);
-        })  
-        ->withMiddleware(function (Middleware $middleware) {
-            $middleware->validateCsrfTokens(except: [
-                'stripe/*',
-                'http://example.com/foo/bar',
-                'http://example.com/foo/*',
-            ]);
-        })
+        })->create();  
+       
     /*
     ->withExceptions(function (Exceptions $exceptions) {
-        //https://special-funicular-pj7qr4qv9r9x3vpq-8001.app.github.dev/postData
+       
     
-    })->create();
-*/;
+    })->create();*/
